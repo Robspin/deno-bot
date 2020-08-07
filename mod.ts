537b3aa299f928c054c8c96b07d68df7ce2fccb3
Robin Steeman <Robin_steeman@hotmail.com>
2020-08-07 12:25:33 +0200

    Added hmac sha256 function to hash private key

--- mod.ts
+++ mod.ts
@@ -1,5 +1,16 @@
 import * as log from 'https://deno.land/std/log/mod.ts';
 import { config } from 'https://deno.land/x/dotenv/mod.ts';
+// import { sha256 } from 'https://deno.land/x/sha256@v1.0.2/mod.ts';
+import { hmac } from 'https://deno.land/x/hmac@v1.0.2/mod.ts';
 
 const APIKEY: string = config().APIKEY;
 const SECRETKEY: string = config().SECRETKEY;
+
+const endPoint: string = '/api/v3/account';
+const dataQueryString: string = 'recvWindow=20000&timestamp=' + Date.now();
+
+const signature = hmac('sha256', SECRETKEY, dataQueryString, 'utf8', 'hex');
+
+const URL: string = `https://api.binance.com${endPoint}?${dataQueryString}&signature=${signature}`;
+
+console.log(URL);
