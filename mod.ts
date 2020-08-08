2391e0caebae58ac344fe47f89854609e3949d70
Robin Steeman <Robin_steeman@hotmail.com>
2020-08-08 20:54:56 +0200

    Changed accountdata fetching function, started on websocket

--- mod.ts
+++ mod.ts
@@ -1,6 +1,5 @@
 import * as log from 'https://deno.land/std/log/mod.ts';
 import { config } from 'https://deno.land/x/dotenv/mod.ts';
-// import { sha256 } from 'https://deno.land/x/sha256@v1.0.2/mod.ts';
 import { hmac } from 'https://deno.land/x/hmac@v1.0.2/mod.ts';
 
 const APIKEY: string = config().APIKEY;
@@ -13,4 +12,18 @@ const signature = hmac('sha256', SECRETKEY, dataQueryString, 'utf8', 'hex');
 
 const URL: string = `https://api.binance.com${endPoint}?${dataQueryString}&signature=${signature}`;
 
-console.log(URL);
+// log.info(URL);
+
+async function getAccountData() {
+   log.info('Fetching Acount Data...');
+
+   return await fetch(URL, {
+      method: 'GET',
+      headers: { 'X-MBX-APIKEY': APIKEY }
+   })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch((err: any) => console.log(err));
+}
+
+console.log(getAccountData());
