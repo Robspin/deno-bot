ad0ad1ef5645540b4b7b1042fc172007358fc277
Robin Steeman <Robin_steeman@hotmail.com>
2020-08-10 10:07:54 +0200

    Implemented basic websocket connection

---
+++ webSocket.ts
@@ -0,0 +1,13 @@
+import { WebSocket } from 'https://deno.land/x/websocket/mod.ts';
+
+const binanceSocket: WebSocket = new WebSocket(
+   'wss://stream.binance.com:9443/ws/btcusdt@trade'
+);
+
+binanceSocket.on('open', function () {
+   console.log('ws connected!');
+});
+
+binanceSocket.on('message', function (event: object) {
+   console.log(event);
+});
